e36e281d5f6e0819d862986584abf842f9f124fc
Егор <egorkaraz@gmail.com>
2018-08-11 16:09:24 +0300

    task

--- task.py
+++ task.py
@@ -1,4 +1,14 @@
-def task(a):
-    result = a + a
-    print(result)
-    print(result, 're')
+import time
+
+
+if __name__ == '__main__':
+    a = 0
+    with open('sonm_test.txt', 'w') as f:
+        while True:
+            b = a * 5
+            result = a + b
+            print('%i + %i = %i' % (a, b, result))
+            f.write('%i + %i = %i\n' % (a, b, result))
+
+            time.sleep(1)
+            a += 1
